1ee5733295f53fff2d7383b4f31644d81460b8b8
Arne Tripolone <arnetripolone@gmail.com>
2021-08-20 11:57:35 +1000

    Inquirer question array added and working

--- index.js
+++ index.js
@@ -8,56 +8,102 @@ require('dotenv').config();
 inquirer
   .prompt([
     { 
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
-    },
-    {
       type: 'list',
-      name: 'occupation',
-      message: 'What is your occupation?',
-      choices: ["Manager", "Intern", "Engineer"],
+      name: 'optionselect',
+      message: 'What would you like to do?',
+      choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"],
     },
     {
       type: "input",
-      name: "github",
-      message: "Enter your github username:",
+      name: "department",
+      message: "Enter the department name",
       when: (answers) => {
-          if (answers.occupation === "Engineer") {
+          if (answers.optionselect === "add a department") {
               return true;
           }
       }
     }, 
     {
       type: "input",
-      name: "school",
-      message: "Enter your school name:",
+      name: "addrole",
+      message: "Enter role name:",
       when: (answers) => {
-          if (answers.occupation === "Intern") {
+          if (answers.optionselect === "add a role") {
               return true;
           }
       }
     },
     {
       type: "input",
-      name: "office_number",
-      message: "Enter your office number:",
+      name: "salary",
+      message: "Enter role salary:",
       when: (answers) => {
-          if (answers.occupation === "Manager") {
+          if (answers.addrole) {
               return true;
           }
       }
     },    
     {
       type: "input",
-      name: 'id',
-      message: 'What is your ID?',
+      name: "adddepartment",
+      message: "Enter role department:",
+      when: (answers) => {
+          if (answers.salary) {
+              return true;
+          }
+      }
     },
     {
       type: "input",
-      name: 'email',
-      message: 'What is your email address?',
-    },       
+      name: "addemployee",
+      message: "Enter employee first name:",
+      when: (answers) => {
+          if (answers.optionselect === "add an employee") {
+              return true;
+          }
+      }
+    },
+    {
+      type: "input",
+      name: "lastname",
+      message: "Enter employee last name:",
+      when: (answers) => {
+          if (answers.addemployee) {
+              return true;
+          }
+      }
+    },
+    {
+      type: "input",
+      name: "role",
+      message: "Enter employee role:",
+      when: (answers) => {
+          if (answers.lastname) {
+              return true;
+          }
+      }
+    },
+    {
+      type: "input",
+      name: "manager",
+      message: "Enter employee manager:",
+      when: (answers) => {
+          if (answers.role) {
+              return true;
+          }
+      }
+    },
+    {
+      type: "list",
+      name: "updateemployee",
+      message: "Select employee to update",
+      choices: ["not sure how to do this one exactly"],
+      when: (answers) => {
+          if (answers.optionselect === "update an employee role") {
+              return true;
+          }
+      }
+    },                      
   ])
   
   .then((answers) => {
